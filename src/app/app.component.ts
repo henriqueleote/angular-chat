535547c47dd85eb655dc1353b34ce0989f619f7f
Henrique Leote <henriqueleote@outlook.com>
2023-03-01 19:10:59 +0000

    Code with problems
    err_connection_refused from port 8000

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import Pusher from 'pusher-js';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-chat';
+export class AppComponent implements OnInit {
+  username = 'username';
+  message = '';
+  messages = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    Pusher.logToConsole = true;
+
+    const pusher = new Pusher('41308c1539bf75b0e134', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('chat');
+    channel.bind('message', data => {
+      this.messages.push(data);
+    });
+  }
+
+  submit(): void {
+    this.http.post('http://localhost:8000/api/messages', {
+      username: this.username,
+      message: this.message
+    }).subscribe(() => this.message = '');
+  }
 }
